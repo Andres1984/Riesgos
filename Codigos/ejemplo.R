7772b6dfd50edafc9b8a6c548e98c55575f06ed2
Andrés <andres.marfh@gmail.com>
2020-08-26 11:45:16 -0500

    matrizvarcoavr

---
+++ Codigos/ejemplo.R
@@ -0,0 +1,18 @@
+## Como crear la matriz varcovar para el portafolio
+#Vol ewma
+ewa=0.1 ## Vol ewma
+ewb=0.3
+ewc=0.12
+
+pab=0.65 # Correlacion
+pac=0.53
+pbc=0.20
+
+#Se crea el vector
+
+a=c(ewa*ewa,pab*ewa*ewb,pac*ewa*ewc,pab*ewa*ewb,ewb*ewb,pbc*ewb*ewc,pac*ewa*ewc,pbc*ewb*ewc,ewc*ewc)
+
+#Matriz Varcovar
+
+cov=matrix(a,nrow=3,ncol=3)
+cov*20
