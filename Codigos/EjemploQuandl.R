a6a2e15642e30a08f0729ccc99ebf8188cade03a
Andrés <andres.marfh@gmail.com>
2020-09-16 14:13:58 -0500

    Quandl

---
+++ Codigos/EjemploQuandl.R
@@ -0,0 +1,13 @@
+## Proceso básico para descargar los precios de Quandl
+
+library(Quandl)
+CL3=Quandl("CHRIS/CME_GC3", api_key="zxdSEzha_e_UwhD8Pgdw",type = "xts")
+plot(CL3$Last)
+SI3=Quandl("CHRIS/CME_SI3", api_key="zxdSEzha_e_UwhD8Pgdw",type="xts")
+plot(SI3$Last)
+
+
+CL3=tail(CL3$Last,510)
+SI3=tail(SI3$Last,510)
+
+plot(CL3)
