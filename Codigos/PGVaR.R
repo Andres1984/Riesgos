43bea64e1929ab0e760215f2f800d69a21accac0
Andrés <andres.marfh@gmail.com>
2020-09-23 08:54:00 -0500

    PGVaR

--- Codigos/PGVaR.R
+++ Codigos/PGVaR.R
@@ -81,5 +81,119 @@ jarque.bera.test(x)
 ### Modelo paramétrico a través de la distrobución t Student
 library(QRM)
 res=fit.st(pg)
-library(MASS)
-fitdistr(pg, "t")
+nu=res$par.ests[1]### Grados de libertad
+mut=res$par.ests[2]
+sigma=res$par.ests[3]
+
+# VaR t Student diario
+VaRt99=mut+qt(0.01,nu)*sigma
+VaRt95=mut+qt(0.05,nu)*sigma
+VaRt90=mut+qt(0.1,nu)*sigma
+
+
+
+# VaR t Student Mensual
+
+VaRt99m=mut*20+qt(0.01,nu)*sigma*sqrt(20)
+VaRt95m=mut*20+qt(0.05,nu)*sigma*sqrt(20)
+VaRt90m=mut*20+qt(0.1,nu)*sigma*sqrt(20)
+
+
+# VaR t Student Anual
+
+VaRt99a=mut*250+qt(0.01,nu)*sigma*sqrt(250)
+VaRt95a=mut*250+qt(0.05,nu)*sigma*sqrt(250)
+VaRt90a=mut*250+qt(0.1,nu)*sigma*sqrt(250)
+
+
+###  VaR Simulación Montecarolo ( Movimiento Geométrico Browniano)
+
+
+set.seed(123)# Número Pseudo aleatorios
+caminatas <- function(s0, mu, sigma, nsims, periods) 
+{
+  # So precio spot
+  # mu tendencia (promedio de los rendimientos)
+  # sigma Volatilidad de los rendimientos
+  # nsim número de simulaciones
+  # periods Es un vector
+  
+  s0 = as.vector(s0)
+  nsteps = length(periods)
+  dt = c(periods[1], diff(periods))
+  
+  if( length(s0) == 1 ) {
+    drift = mu - 0.5 * sigma^2
+    if( nsteps == 1 ) {
+      s0 * exp(drift * dt + sigma * sqrt(dt) * rnorm(nsims))
+    } else {
+      temp = matrix(exp(drift * dt + sigma * sqrt(dt) * rnorm(nsteps * nsims)), nc=nsims)
+      for(i in 2:nsteps) temp[i,] = temp[i,] * temp[(i-1),]
+      s0 * temp
+    }
+  } else {
+    require(MASS)
+    drift = mu - 0.5 * diag(sigma)
+    n = length(mu)
+    
+    if( nsteps == 1 ) {
+      s0 * exp(drift * dt + sqrt(dt) * t(mvrnorm(nsims, rep(0, n), sigma)))
+    } else {
+      temp = array(exp(as.vector(drift %*% t(dt)) + t(sqrt(dt) * mvrnorm(nsteps * nsims, rep(0, n), sigma))), c(n, nsteps, nsims))
+      for(i in 2:nsteps) temp[,i,] = temp[,i,] * temp[,(i-1),]
+      s0 * temp
+    }
+  }
+  
+  
+  
+}
+
+nsims=50000
+periods=0:20
+
+
+PGMB=caminatas(as.numeric(tail(PG$PG.Close,1)),mpg,sdpg,nsims,periods)
+matplot(periods,PGMB[,1:10000],type = "l",ylab = "Precio", xlab = "Tiempo", main=" Escenarios PG")
+
+
+
+EV=function(ULD){
+  n= dim(ULD)[1]
+  
+  # Matriz de simulaciones  
+  require(dplyr)
+  EVUL=rowMeans(ULD)
+  VOLUL=sqrt(rowMeans(ULD^{2})-EVUL^{2})#V[x]=E[x^2]-(E[x])^2
+  ULEV=as.data.frame(EVUL)
+  ULEV=mutate(ULEV, VOLULS =EVUL+VOLUL)
+  ULEV=mutate(ULEV, VOLULI =EVUL-VOLUL)
+  ULEV[1,2:3]=EVUL[1]
+  
+  TUL=t(ULEV)
+  TUL[,1]=EVUL[1]
+  VAR0.05=rep(0,n-1)
+  for (i in 1:dim(TUL)[2]){
+    VAR0.05[i]=quantile(TUL[,i],0.05)
+  }
+  ULEV$VAR0.05=VAR0.05
+  VAR0.01=rep(0,n-1)
+  for (i in 1:dim(TUL)[2]){
+    VAR0.01[i]=quantile(TUL[,i],0.01)
+  }
+  ULEV$VAR0.01=VAR0.01
+  
+  return(ULEV)
+}
+
+
+
+PGEV=EV(PGMB)
+
+
+matplot(PGEV[,],type = "l",ylab = "Precio", xlab = "Tiempo", main=" Escenarios PG")
+
+
+hoy=3700*PGEV[1,4]*20
+VaR95Gbm=3700*PGEV[21,4]*20
+Perdida=VaR95Gbm-hoy
