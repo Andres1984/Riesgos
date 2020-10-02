c6335a4ddf1d69d77c43893a8b5ed9103d3dc91b
Andrés <andres.marfh@gmail.com>
2020-10-02 11:03:05 -0500

    WMT

--- Codigos/WMT.R
+++ Codigos/WMT.R
@@ -109,7 +109,7 @@ InvInicial=WMT$WMT.Close[252]*20
 InvInicial
 VaRTK=cbind(VaR99,VaR95,VaR90,VaR99t,VaR95t,VaR90t,VaR[1],VaR[2],VaR[3])*as.numeric(InvInicial)
 WMTTK=WMT$WMT.Close*20 # Número de acciones
-dWMTTK=diff(WMTTK)
+
 colnames(WMTTK)="Inversion"
 plot(WMTTK)
 WMTTK$VaR1N=WMTTK$Inversion*VaR99
@@ -122,31 +122,154 @@ WMTTK$VaR1H=WMTTK$Inversion*VaR[1]
 WMTTK$VaR5H=WMTTK$Inversion*VaR[2]
 WMTTK$VaR10H=WMTTK$Inversion*VaR[3]
 
+dWMTTK=diff(WMTTK$Inversion)
 
 
-dim(WMTTK)
 
 dWMTTK=dWMTTK[-1]
 WMTTTK=WMTTK[,2:10][-1]
 
 
 
-  TK=NULL
-  TK$N99=ifelse(WMTTTK$VaR1N>=dWMTTK, 1, 0)
-  TK$N95=ifelse(WMTTTK$VaR5N>=dWMTTK, 1, 0)
-  TK$N90=ifelse(WMTTTK$VaR10N>=dWMTTK, 1, 0)
-  TK$T99=ifelse(WMTTTK$VaR1T>=dWMTTK, 1, 0)
-  TK$T95=ifelse(WMTTTK$VaR5T>=dWMTTK, 1, 0)
-  TK$T90=ifelse(WMTTTK$VaR10T>=dWMTTK, 1, 0)
-  TK$H99=ifelse(WMTTTK$VaR1H>=dWMTTK, 1, 0)
-  TK$H95=ifelse(WMTTTK$VaR5H>=dWMTTK, 1, 0)
-  TK$H90=ifelse(WMTTTK$VaR10H>=dWMTTK, 1, 0)
-  TK=as.data.frame(TK)
+TK=NULL
+TK$N99=ifelse(WMTTTK$VaR1N>dWMTTK,1,0)
+TK$N95=ifelse(WMTTTK$VaR5N>=dWMTTK, 1, 0)
+TK$N90=ifelse(WMTTTK$VaR10N>=dWMTTK, 1, 0)
+TK$T99=ifelse(WMTTTK$VaR1T>=dWMTTK, 1, 0)
+TK$T95=ifelse(WMTTTK$VaR5T>=dWMTTK, 1, 0)
+TK$T90=ifelse(WMTTTK$VaR10T>=dWMTTK, 1, 0)
+TK$H99=ifelse(WMTTTK$VaR1H>=dWMTTK, 1, 0)
+TK$H95=ifelse(WMTTTK$VaR5H>=dWMTTK, 1, 0)
+TK$H90=ifelse(WMTTTK$VaR10H>=dWMTTK, 1, 0)
+TK=as.data.frame(TK)
 
+  
 RTK=colSums(TK)  
-colnames(TK)=names(WMTTTK)  
 RTK=as.data.frame(RTK)
-colnames(RTK)=names(WMTTTK) 
+
+
+#### Movimiento geométrico Browniano
+
+
+set.seed(123)# Número Pseudo aleatorios
+caminatas <- function(s0, mu, sigma, nsims, periods) 
+{
+  # So precio spot
+  # mu tendencia (promedio de los rendimientos)
+  # sigma Volatilidad de los rendimientos
+  # nsim número de simulaciones
+  # periods Es un vector
+  
+  s0 = as.vector(s0)
+  nsteps = length(periods)
+  dt = c(periods[1], diff(periods))
+  
+  if( length(s0) == 1 ) {
+    drift = mu - 0.5 * sigma^2
+    if( nsteps == 1 ) {
+      s0 * exp(drift * dt + sigma * sqrt(dt) * rnorm(nsims))
+    } else {
+      temp = matrix(exp(drift * dt + sigma * sqrt(dt) * rnorm(nsteps * nsims)), nc=nsims)
+      for(i in 2:nsteps) temp[i,] = temp[i,] * temp[(i-1),]
+      s0 * temp
+    }
+  } else {
+    require(MASS)
+    drift = mu - 0.5 * diag(sigma)
+    n = length(mu)
+    
+    if( nsteps == 1 ) {
+      s0 * exp(drift * dt + sqrt(dt) * t(mvrnorm(nsims, rep(0, n), sigma)))
+    } else {
+      temp = array(exp(as.vector(drift %*% t(dt)) + t(sqrt(dt) * mvrnorm(nsteps * nsims, rep(0, n), sigma))), c(n, nsteps, nsims))
+      for(i in 2:nsteps) temp[,i,] = temp[,i,] * temp[,(i-1),]
+      s0 * temp
+    }
+  }
+  
+  
+  
+}
+
+
+### Proyección de precios
+
+nsims=10000
+periods=0:250
+
+
+WMTSM=caminatas(as.numeric(tail(WMT$WMT.Close,1)),mwmt,sdwmt,nsims,periods )
+matplot(periods,WMTSM[,1:10000], type='l', xlab='días',ylab='Precios',main='Escenarios Wallmart')
+
+EV=function(ULD){
+  n= dim(ULD)[1]
+  
+  # Matriz de simulaciones  
+  require(dplyr)
+  EVUL=rowMeans(ULD)
+  VOLUL=sqrt(rowMeans(ULD^{2})-EVUL^{2})#V[x]=E[x^2]-(E[x])^2
+  ULEV=as.data.frame(EVUL)
+  ULEV=mutate(ULEV, VOLULS =EVUL+VOLUL)
+  ULEV=mutate(ULEV, VOLULI =EVUL-VOLUL)
+  ULEV[1,2:3]=EVUL[1]
+  
+  TUL=t(ULEV)
+  TUL[,1]=EVUL[1]
+  VAR0.05=rep(0,n-1)
+  for (i in 1:dim(TUL)[2]){
+    VAR0.05[i]=quantile(TUL[,i],0.05)
+  }
+  ULEV$VAR0.05=VAR0.05
+  VAR0.01=rep(0,n-1)
+  for (i in 1:dim(TUL)[2]){
+    VAR0.01[i]=quantile(TUL[,i],0.01)
+  }
+  ULEV$VAR0.01=VAR0.01
+  
+  return(ULEV)
+}
+
+
+WMTEV=EV(WMTSM)
+
+
+matplot(WMTEV[,], type='l', xlab='días', ylab='Precios',main="Escenarios Wallmart" , cex= 10.8 )
 
 
 
+#### Test de Kupiec ### backtesting
+
+
+WMTSMTK=caminatas(as.numeric(head(WMT$WMT.Close,1)),mwmt,sdwmt,nsims,periods )
+WMTEVTK=EV(WMTSMTK)
+WMTEVTK$HISTORICO=WMT$WMT.Close[1:251]
+matplot(WMTEVTK[,], type='l', xlab='días', ylab='Precios',main="Test de Kupiec Wallmart" , cex= 10.8 )
+
+# Hasta este punto se trabaja con redimientos
+EWMAwmt=cbind(wmt)
+colnames(EWMAwmt)="WMT"
+lambda=0.95
+EWMAwmt$l=0
+
+l=rep(0,length(EWMAwmt[,2]))### Columnas de ceros
+
+for  (i in length(EWMAwmt[,2]):1){
+  
+  l[i]=lambda^(i-1)  ##
+  
+}
+
+l=sort(l,decreasing = FALSE)
+EWMAwmt$l=l
+EWMAwmt$B=EWMAwmt[,1]^2
+EWMAwmt$C=EWMAwmt$l*EWMAwmt$B
+volwmt=sqrt(sum(EWMAwmt$C)*(1-lambda))
+
+WMTSMTKE=caminatas(as.numeric(head(WMT$WMT.Close,1)),mwmt,sdwmt,nsims,periods )
+WMTEVTKE=EV(WMTSMTKE)
+WMTEVTKE$HISTORICO=WMT$WMT.Close[1:251]
+
+
+par(mfrow=c(2,1))
+matplot(WMTEVTK[,], type='l', xlab='días', ylab='Precios',main="Test de Kupiec Wallmart" , cex= 10.8 )
+matplot(WMTEVTKE[,], type='l', xlab='días', ylab='Precios',main="Test de Kupiec EWMA Wallmart" , cex= 10.8 )
