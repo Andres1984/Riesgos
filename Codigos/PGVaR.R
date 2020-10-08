601256dc39fba1a1c9a9ecd9c5ff2311008952dd
Andrés <andres.marfh@gmail.com>
2020-10-08 11:18:03 -0500

    PGVaR

--- Codigos/PGVaR.R
+++ Codigos/PGVaR.R
@@ -20,6 +20,19 @@ VaR95*10000
 VaR90*10000
 
 
+### ES FB
+
+p = c(0.1,0.05,0.01) 
+VaR = qnorm(p)
+ES = -dnorm(qnorm(p))/p
+ES
+ES90=mpg+ES[1]*sdpg### ES al 90% de Nivel de confianza
+ES95=mpg+ES[2]*sdpg### ES al 95% de Nivel de confianza
+ES99=mpg+ES[3]*sdpg### ES al 99% de Nivel de confianza
+ES99*10000
+ES95*10000
+ES90*10000
+
 ### VaR mensual Primer método Paramétrico
 
 VaR99m=mpg*20+qnorm(0.01)*sdpg*sqrt(20)
@@ -52,10 +65,12 @@ VaR90a*10000
 
 # Proceso diario
 VaR=quantile(pg,probs=c(0.01,0.05,0.1))
+ES=quantile(pg,probs=c(0.005,0.025,0.05))
 VaR ### Diario
 p=density(pg)
 hist(pg,col="blue",breaks=30, freq = FALSE)
-abline(v=VaR, col="red")
+abline(v=VaR, col="red",cex=1.4)
+abline(v=ES, col="green")
 lines(p,col="red")
 
 # Proceso Mensual
@@ -90,7 +105,21 @@ VaRt99=mut+qt(0.01,nu)*sigma
 VaRt95=mut+qt(0.05,nu)*sigma
 VaRt90=mut+qt(0.1,nu)*sigma
 
+VaRt99*10000
+VaRt95*10000
+VaRt90*10000
+p = c(0.1,0.05,0.01) 
+VaRtES = qt(p,nu)
+ES = -dt(p,nu)/p
+ES
 
+ESt99=mut+ES[3]*sigma
+ESt95=mut+ES[2]*sigma
+ESt90=mut+ES[1]*sigma
+
+ESt99*10000
+ESt95*10000
+ESt90*10000
 
 # VaR t Student Mensual
 
@@ -149,7 +178,7 @@ caminatas <- function(s0, mu, sigma, nsims, periods)
   
 }
 
-nsims=50000
+nsims=10000
 periods=0:20
 
 
@@ -197,3 +226,71 @@ matplot(PGEV[,],type = "l",ylab = "Precio", xlab = "Tiempo", main=" Escenarios P
 hoy=3700*PGEV[1,4]*20
 VaR95Gbm=3700*PGEV[21,4]*20
 Perdida=VaR95Gbm-hoy
+
+
+
+### Prueba de Back testing para un período de 255 días ####
+
+
+### Organizando los resultados #####
+
+### Usted desea comprar 20 acciones Posición de la Inversión
+### Vamos a organizar el valor de la inversión diaría en una columna
+### Vamos a organizar el VaR diarío parámetrico normal en otra columna
+### Vamos a organizar el VaR diarío parámetrico t Student en otra columna
+### Vamos a organizar el VaR diarío No parámetrico
+
+## Test de Kupiec TK
+
+
+
+InvInicial=PG$PG.Close[252]*20
+InvInicial
+
+
+PGTK=PG$PG.Close*20###  Usted desea comprar 20 acciones 
+colnames(PGTK)="Inversion"
+plot(PGTK)
+PGTK$VaR1N=PGTK$Inversion*VaR99## VaR paramétrico Dist Normal 99 Nivel de confianza
+PGTK$VaR5N=PGTK$Inversion*VaR95## VaR paramétrico Dist Normal 95 Nivel de confianza
+PGTK$VaR10N=PGTK$Inversion*VaR90## VaR paramétrico Dist Normal 90 Nivel de confianza
+PGTK$VaR1T=PGTK$Inversion*VaRt99 ## VaR paramétrico Dist t Student 99 Nivel de confianza
+PGTK$VaR5T=PGTK$Inversion*VaRt95## VaR paramétrico Dist t Student 95 Nivel de confianza
+PGTK$VaR10T=PGTK$Inversion*VaRt90## VaR paramétrico Dist t Student 90 Nivel de confianza
+PGTK$VaR1H=PGTK$Inversion*VaR[1] ## VaR No paramétrico 99 Nivel de confianza
+PGTK$VaR5H=PGTK$Inversion*VaR[2]  ## VaR No paramétrico  95 Nivel de confianza
+PGTK$VaR10H=PGTK$Inversion*VaR[3] ## VaR No paramétrico 90 Nivel de confianza
+
+dPGTK=diff(PGTK$Inversion)[-1]
+PGTTK=PGTK[,2:10][-1]### Solo está el VaR
+
+TK=NULL
+TK$N99=ifelse(PGTTK$VaR1N>dPGTK,1,0)
+TK$N95=ifelse(PGTTK$VaR5N>dPGTK,1,0)
+TK$N90=ifelse(PGTTK$VaR10N>dPGTK,1,0)
+TK$T99=ifelse(PGTTK$VaR1T>dPGTK,1,0)
+TK$T95=ifelse(PGTTK$VaR5T>dPGTK,1,0)
+TK$T90=ifelse(PGTTK$VaR10T>dPGTK,1,0)
+TK$H99=ifelse(PGTTK$VaR1H>dPGTK,1,0)
+TK$H95=ifelse(PGTTK$VaR5H>dPGTK,1,0)
+TK$H90=ifelse(PGTTK$VaR10H>dPGTK,1,0)
+
+TK=as.data.frame(TK)
+RTK=colSums(TK)
+colnames(TK)=names(PGTTK)  
+RTK=as.data.frame(RTK)
+
+
+
+
+### Test de Kupiec Movimiento Geomértico
+
+
+nsims=10000
+periods=0:251
+
+
+PGMBTK=caminatas(as.numeric(head(PG$PG.Close,1)),mpg,sdpg,nsims,periods)
+PGEVTK=EV(PGMBTK)
+PGEVTK$HISTORICO=PG$PG.Close
+matplot(PGEVTK,type = "l",ylab = "Precio", xlab = "Tiempo", main=" Test de Kupiec PG")
